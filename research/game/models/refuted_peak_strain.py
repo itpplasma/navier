e20531983f7bae4d422740b9c13819c6f2d397eb
Christopher Albert <albert@tugraz.at>
2026-09-12 08:53:23 +0200

    Remove redundant executable research game

--- research/game/models/refuted_peak_strain.py
+++
@@ -1,6 +0,0 @@
-def predict(control):
-    if control["id"]=="clock-action-obstruction":
-        return {"fixed_action_requires":"peak strain comparable to clean viscous rate","slow_purifier_requires":"lambda*b >= eta0","bounded_fast_parent_requires":"lambda*sqrt(S) <= C0","all_three_with_N_over_b_to_infinity":True}
-    if control["id"]=="old-strong-scaling-action":
-        return {"integrated_clean_action_limit":"positive","sufficient_for_fixed_positive_filter_action":True}
-    return control["observation"]
